ac8c3b569c128e09018831c8a84dae9aaf391bcc
Kshirakura <49435807+Kshirakura@users.noreply.github.com>
2021-07-28 20:36:20 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,8 +8,48 @@ $(function() {
     // $('#login-modal #submit-btn').css('background', '#102542');
   });
 
+  $('.menu-show').click(function() {
+    $('#menu-modal').animate({
+      left: 0
+  },300);
+  });
+
+  $('.go-top').click(function() {
+    $('html, body').animate({
+      scrollTop:0
+    }, 300);
+  });
+
+  $('.scroll-btn').click(function() {
+    var id = $(this).attr('href');
+    var position =$(id).offset().top;
+    $('html, body').animate({
+      scrollTop:position
+    }, 300);
+  });
+
+
+
   $('.close-btn').click(function() {
     $('#signup-modal').fadeOut();
     $('#login-modal').fadeOut();
+    $('#menu-modal').animate({
+      left: -300
+  },300);
   });
+
+  $('.site-title').hover(function() {
+    $(this).animate({
+      opacity : 0.8
+  },100);
+  },
+  function() {
+    $(this).animate({
+      opacity : 1.0
+  },100);
+  }
+  );
+
+  
+
 });
