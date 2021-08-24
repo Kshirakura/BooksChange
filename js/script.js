aa8ea913bb3478c65ef278ed120695830db10a9d
Kshirakura <49435807+Kshirakura@users.noreply.github.com>
2021-08-25 03:58:40 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function() {
+
+  // ie判定
+  var userAgent = window.navigator.userAgent.toLowerCase();
+  if(userAgent.indexOf('msie') != -1 ||
+        userAgent.indexOf('trident') != -1) {
+          $('.twitter-share-button').hide();
+        }
   
   $('.signup-show').click(function() {
     $('#signup-modal').fadeIn();
